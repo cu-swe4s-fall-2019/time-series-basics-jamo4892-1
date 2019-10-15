1a3b825eee05638ac9eeaf5117a1dd3682bd9eed
Jamie Mothersbaugh <james.mothersbaugh@colorado.edu>
2019-10-15 03:10:35 +0000

    Wrote roundTimeArray & main functions

--- data_import.py
+++ data_import.py
@@ -1,9 +1,11 @@
 import csv
+import sys
 import dateutil.parser
 from os import listdir
 from os.path import isfile, join
 import argparse
 import datetime
+import time
 # Import modules.
 
 
@@ -30,27 +32,42 @@ class ImportData:
 
         self._time = []
         self._value = []
+        self._type = []
+
+        if 'meal' in data_csv or 'bolus' in data_csv or 'activity' in data_csv:
+            self._type = 0
+        if 'basal' in data_csv or 'smbg' in data_csv:
+            self._type = 1
+        if 'cgm' in data_csv or 'hr' in data_csv:
+            self._type = 1
+        # Set data type for use in roundTimeArray function below.
 
         with open(data_csv, "r") as fhandle:
             reader = csv.DictReader(fhandle)
             # Read the file into an array.
             for row in reader:
+                if (row['time'] == ''):
+                    continue
                 try:
-                    self._time.append(dateutil.parser.parse(row['time']))
+                    self._time.append(datetime.datetime.strptime(
+                                      row['time'], '%m/%d/%y %H:%M'))
                     # Append each 'time' value in the 'reader' array to the
                     # '_time' array.
                 except ValueError:
                     print("Input 'time' has bad format")
-                    print(row['time'])
+                    dateutil.parser.parse(row['time'])
                     sys.exit(1)
                     # Find & print any 'time' value that gives a ValueError.
                 self._value.append(row['value'])
+                if (row['value'] == 'low'):
+                    print("Value = 'low'; converting 'value' to 40")
+                    row['value'] == 40
+                if (row['value'] == 'high'):
+                    print("Value = 'high'; converting 'value' to 300")
+                    row['value'] == 300
                 # Append each 'value' value in the 'reader' array to the
                 # '_value' array.
             fhandle.close()
-
-        #self.roundTime(5)
-        # TBD.
 
     def linear_search_value(self, key_time):
         """
@@ -72,74 +89,150 @@ class ImportData:
         value = []
         for i in range(len(self._time)):
             if self._time[i] == key_time:
-                value.append(self_.value[i])
+                value.append(self._value[i])
                 # If the input key time is in the '_time' array,
                 # append the _value value of the key to the 'value' array.
-
             if len(value) == 0:
                 print('No values found for the selected key_time')
                 return -1
+        return value
 
 
 def roundTimeArray(obj, res):
     """
     Purpose
     -------
-    
+    This function rounds an array of time values to the nearest 5-minute value,
+    then returns the rounded time values and the data values of the rounded
+    time values in a zipped iterable array.
+
     Inputs
     ------
-    Obj:
-    
+    Obj: Object
+    Object from ImportData class.
+
     Res: Integer
     Resolution value for rounding time.
-    
+
     Outputs
     -------
-    
+    Array
+    Zipped array with the rounded time and data values.
     """
-    
-    # Inputs: obj (ImportData Object) and res (rounding resoultion)
-    # objective:
-    # create a list of datetime entries and associated values with the times rounded to the nearest rounding resolution (res)
-    # ensure no duplicated times
-    # handle duplicated values for a single timestamp based on instructions in the assignment
-    # return: iterable zip object of the two lists
-    # note: you can create additional variables to help with this task which are not returned
+
+    round_time = []
+    round_values = []
+    for i in range(len(obj._time)):
+        minutes_under = datetime.timedelta(minutes=(obj._time[i].minute % res),
+                                           seconds=(obj._time[i].second))
+        minutes_over = datetime.timedelta(minutes=res) - minutes_under
+        # Find how many minutes over or under the nearest 5 minutes the current
+        # time value is.
+        if (obj._time[i].minute % res) <= res/2.0:
+            newtime = obj._time[i] - minutes_under
+        else:
+            newtime = obj._time[i] + minutes_over
+        round_time.append(newtime.strftime("%m/%d/%Y %H:%M"))
+        # Round each time value up or down to the nearest 5 minutes, then
+        # replace  the time value in the ImportData object with the #
+        # rounded time.
+
+    for j in range(len(obj._time)):
+        if obj._time[j] == obj._time[j - 1]:
+            continue
+        # Skip the loop if there is a duplicate time value.
+
+        value_search = obj.linear_search_value(obj._time[j])
+        # Search for the value at each rounded time in the ImportData 'value'
+        # array,  then add the value to the 'round_values' array. Duplicate
+        # values are either summed or averaged, depending on the input data
+        # type.
+
+    round_values = obj._value
+    # The linear_search_value function cannot find the values in the ImportData
+    # object. I don't know why. I am setting 'round_values' to be the values
+    # already found in the ImportData object.
+
+    return zip(round_time, round_values)
 
 
 def printArray(data_list, annotation_list, base_name, key_file):
-    # combine and print on the key_file
+    """
+    Purpose
+    -------
+    Combine and print on the key_file
+
+    Inputs
+    ------
+    data_list a list of zip objects of data (time, value) pairs
+    annotation_list a list of strings with column labels for the data value
+    base_name the file name you want to print as
+    key_file the name from annotation_list you want to align the data on
+
+    Outputs
+    -------
+    """
+
 
 if __name__ == '__main__':
+    """
+    Main function
+    """
 
-    #adding arguments
-    parser = argparse.ArgumentParser(description= 'A class to import, combine, and print data from a folder.',
-    prog= 'dataImport')
-
-    parser.add_argument('folder_name', type = str, help = 'Name of the folder')
-
-    parser.add_argument('output_file', type=str, help = 'Name of Output file')
-
-    parser.add_argument('sort_key', type = str, help = 'File to sort on')
-
-    parser.add_argument('--number_of_files', type = int,
-                        help = "Number of Files", required = False)
-
+    parser = argparse.ArgumentParser(description='A class to import,' +
+                                     'combine, and print data from a folder',
+                                     prog='dataImport')
+    parser.add_argument('--folder_name', type=str, help='Name of input folder')
+    parser.add_argument('--output_file', type=str, help='Name of output file')
+    parser.add_argument('--sort_key', type=str, help='File to sort on')
     args = parser.parse_args()
+    # Input arguments.
 
+    try:
+        files_lst = listdir(args.folder_name)
+    except (FileNotFoundError, NameError):
+        print('Cannot find specified directory')
+        sys.exit(1)
+    files_lst.remove('.ipynb_checkpoints')
+    # List the files found in the specified folder.
 
-    #pull all the folders in the file
-    files_lst = # list the folders
-
-
-    #import all the files into a list of ImportData objects (in a loop!)
     data_lst = []
+    for f in files_lst:
+        data_lst.append(ImportData(args.folder_name + '/' + f))
+        # Read each file with ImportData.
+    if (len(data_lst) == 0):
+        print('No files read into ImportData class')
+        sys.exit(1)
 
-    #create two new lists of zip objects
-    # do this in a loop, where you loop through the data_lst
-    data_5 = [] # a list with time rounded to 5min
-    data_15 = [] # a list with time rounded to 15min
+    start_time = time.time()
+    data_5 = []
+    for obj in data_lst:
+        data_5.append(roundTimeArray(obj, 5))
+    end_time = time.time()
+    print('Time to create data_5:')
+    print(end_time - start_time)
+    # Use roundTimeArray to create a zipped list with times rounded to
+    # 5 minutes, and print the length of time it takes Python to do this.
 
-    #print to a csv file
-    printLargeArray(data_5,files_lst,args.output_file+'_5',args.sort_key)
-    printLargeArray(data_15, files_lst,args.output_file+'_15',args.sort_key)
+    start_time = time.time()
+    data_15 = []
+    for obj in data_lst:
+        data_15.append(roundTimeArray(obj, 15))
+    end_time = time.time()
+    print('Time to create data_15:')
+    print(end_time - start_time)
+    # Use roundTimeArray to create a zipped list with times rounded to
+    # 15 minutes, and print the length of time it takes Python to do this.
+    sys.exit()
+    
+    # I was not able to write the printLargeArray function. The script exits
+    # before reaching this block.
+    res1 = printLargeArray(data_5, files_lst, args.output_file+'_5',
+                           args.sort_key)
+    if (res1 == -1):
+        sys.exit(1)
+
+    res2 = printLargeArray(data_15, files_lst, args.output_file+'_15',
+                           args.sort_key)
+    if (res2 == -1):
+        sys.exit(1)
