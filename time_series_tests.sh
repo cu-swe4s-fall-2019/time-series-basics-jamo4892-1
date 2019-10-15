10269735434692cc615bb8af63083f4c915a5259
Jamie Mothersbaugh <james.mothersbaugh@colorado.edu>
2019-10-15 04:02:59 +0000

    Made edits to functional tests

--- time_series_tests.sh
+++ time_series_tests.sh
@@ -11,7 +11,7 @@ assert_no_stdout
 # Run pycodestyle on data_import.py- assert these both pass with no style errors.
 
 run roundTimeArray_function_test python data_import.py --folder_name smallData --output_file test.txt --sort_key cgm_small.csv
-assert_stdout
+assert_no_stdout
 assert_exit_code 0
 # Run data_import.py on the 'cgm_small.csv' file, as required by the assignment. Assert the script 
-# has stdout and exits with 0.
+# has no stdout and exits with 0.
